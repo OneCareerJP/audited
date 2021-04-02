d22014afdd4240c68bdf431b19ac4ef7806c87c7
nakatomotoi <poilkjmnb0824@gmail.com>
2021-04-02 16:00:17 +0900

    change sql delimiter

--- lib/audited/query.rb
+++ lib/audited/query.rb
@@ -2,7 +2,7 @@ module Query
 	def exec_query(sql, name = "SQL", binds = [], prepare: false)
 		old_connected_sql = ::Audited.store[:sql]
 		if old_connected_sql
-			::Audited.store[:sql] = old_connected_sql + " / " + sql
+			::Audited.store[:sql] = old_connected_sql + ";" + sql
 		else
 			::Audited.store[:sql] = sql
 		end
