a19c74c88929411bb87f4667e0a5fff90e25aeab
Kohei.Yoshida <kohei.yoshida@onecareer.jp>
2022-11-10 20:16:26 +0900

    TEST9

--- lib/audited.rb
+++ lib/audited.rb
@@ -51,7 +51,7 @@ end
 ActiveSupport.on_load(:after_initialize) do
   adapters = ActiveRecord::ConnectionAdapters.constants.select{|klass| klass.to_s.include?("Adapter")}
   for klass in adapters do
-    puts klass
+    puts "::Audited.enable_sql_log: #{::Audited.enable_sql_log}"
     ::ActiveRecord::ConnectionAdapters.const_get(klass).send :prepend, Query
   end
 end
